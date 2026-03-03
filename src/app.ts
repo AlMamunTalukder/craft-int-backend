c6714466015fe2f2cc6a95ec7bd1004219fea923
AlMamunTalukder <almamuntalukder0@gmail.com>
2026-03-03 13:40:41 +0600

    push code for velidation

--- src/app.ts
+++ src/app.ts
@@ -39,8 +39,12 @@ app.use(
   }),
 );
 // Parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
