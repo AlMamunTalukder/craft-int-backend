3bb297df4695034cd92908a5b6eef882daa680a7
AlMamunTalukder <almamuntalukder0@gmail.com>
2026-08-13 23:42:03 +0600

    add routine, new feature and upgrade code, update deploy.yml

--- src/app/modules/asset/route.ts
+++ src/app/modules/asset/route.ts
@@ -1,30 +1,35 @@
-import express from 'express';
-import { validateRequest } from '../../middlewares/validateRequest';
-import { auth } from '../../middlewares/auth';
-import { assetControllers } from './controller';
-import { assetValidations } from './validation';
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { auth } from "../../middlewares/auth";
+import { assetControllers } from "./controller";
+import { assetValidations } from "./validation";
 
 const router = express.Router();
 
-router.get('/summary', assetControllers.getSummary);
+//asset routes
+router.get("/summary", assetControllers.getSummary);
 
 router.post(
-  '/',
-  auth('admin', 'super_admin'),
+  "/",
+  auth("admin", "super_admin"),
   validateRequest(assetValidations.createAssetValidation),
   assetControllers.createAsset,
 );
 
-router.get('/', assetControllers.getAllAssets);
-router.get('/:id', assetControllers.getSingleAsset);
+router.get("/", assetControllers.getAllAssets);
+router.get("/:id", assetControllers.getSingleAsset);
 
 router.patch(
-  '/:id',
-  auth('admin', 'super_admin'),
+  "/:id",
+  auth("admin", "super_admin"),
   validateRequest(assetValidations.updateAssetValidation),
   assetControllers.updateAsset,
 );
 
-router.delete('/:id', auth('admin', 'super_admin'), assetControllers.deleteAsset);
+router.delete(
+  "/:id",
+  auth("admin", "super_admin"),
+  assetControllers.deleteAsset,
+);
 
 export const assetRoutes = router;
